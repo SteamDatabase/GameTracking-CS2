760a328ed5b9eff6a6b5b762161be97646bc4e96
Wendy <freeman@steamdb.info>
2017-04-13 20:42:18 +0000

    470 | 1.35.7.4 | 79 files | M  platform/resource/platform_brazilian.txt, M  platform/resource/platform_bulgarian.txt, M  platform/resource/platform_czech.txt, M  platform/resource/platform_danish.txt, M  platform/resource/platform_dutch.txt, M  platform/resource/platform_english.txt, M  platform/resource/platform_finnish.txt, M  platform/resource/platform_french.txt, M  platform/resource/platform_german.txt, M  platform/resource/platform_greek.txt, M  platform/resource/platform_hungarian.txt, M  platform/resource/platform_italian.txt, M  platform/resource/platform_japanese.txt, M  platform/resource/platform_korean.txt, M  platform/resource/platform_koreana.txt, M  platform/resource/platform_norwegian.txt, M  platform/resource/platform_polish.txt, M  platform/resource/platform_portuguese.txt, M  platform/resource/platform_romanian.txt, M  platform/resource/platform_russian.txt, M  platform/resource/platform_schinese.txt, M  platform/resource/platform_spanish.txt, M  platform/resource/platform_swedish.txt, M  platform/resource/platform_tc

--- update.sh
+++ update.sh
@@ -10,18 +10,6 @@ ProcessVPK
 
 mono ../.support/SourceDecompiler/Decompiler.exe -i "csgo/pak01_dir.vpk" -o "csgo/pak01_dir/"
 
-echo "Fixing UCS-2"
-
-while IFS= read -r -d '' file
-do
-	if ! file --mime "$file" | grep "charset=utf-16le"
-	then
-		continue
-	fi
-
-	temp_file=$(mktemp)
-	iconv -t UTF-8 -f UCS-2 -o "$temp_file" "$file" &&
-	mv -f "$temp_file" "$file"
-done <   <(find csgo/ -name "*.txt" -type f -print0)
+FixUCS2
 
 CreateCommit "$(grep "ClientVersion=" csgo/steam.inf | grep -o '[0-9\.]*') | $(grep "PatchVersion=" csgo/steam.inf | grep -o '[0-9\.]*')"
