53b8ae2a5d827a9ef62fb94fd74c1e4ee9587713
Wendy <freeman@steamdb.info>
2017-05-02 23:58:55 +0000

    485 | 1.35.7.7 | 27 files | M  Strings/engine.txt, M  Strings/server.txt, M  Strings/vaudio_miles_client.txt, A  bin/convertdmx.lua, A  bin/dxsupport.cfg, A  bin/dxsupport_episodic.cfg, A  bin/dxsupport_mac.cfg, A  bin/maplist_csgo.txt, A  bin/steam_appid.txt, M  csgo/pakxv_perfectworld_dir.txt, M  csgo/resource/csgo_bulgarian.txt, M  csgo/resource/csgo_czech.txt, M  csgo/resource/csgo_danish.txt, M  csgo/resource/csgo_dutch.txt, M  csgo/resource/csgo_english.txt, M  csgo/resource/csgo_french.txt, M  csgo/resource/csgo_german.txt, M  csgo/resource/csgo_greek.txt, M  csgo/resource/csgo_hungarian.txt, M  csgo/resource/csgo_italian.txt, M  csgo/resource/csgo_portuguese.txt, M  csgo/resource/csgo_romanian.txt, M  csgo/resource/csgo_schinese.txt, M  csgo/resource/csgo_swedish.txt, M  csgo/resource/csgo_tchinese.txt, M  csgo/steam.inf, M  platform/config/network_config.json

---
+++ bin/convertdmx.lua
@@ -0,0 +1,4 @@
+-- Make sure all your paths have forward slashes. (do a search and replace if necessary)
+--NOTE: change the load path and Save path to your own, directories for the DMX you are converting
+Load( "C:/Depot/Valve/trunk/source/cstrike15/content/left4dead2/models/destruction_tanker/dmx/pre_destruction_tanker_trailer.dmx" )
+Save( "C:/Users/brian/Desktop/Decomplie/pre_destruction_tanker_trailer.obj" )
