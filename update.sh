49e93a2b96eeb46b00e099de9c6fbf4e9f875978
Wendy <freeman@steamdb.info>
2018-09-12 00:47:30 +0000

    780 | 1.36.5.3 | 6 files | M bin/linux64/panoramauiclient_client_strings.txt,  M csgo/bin/linux64/client_client_strings.txt,  M csgo/bin/linux64/client_panorama_client_strings.txt,  M csgo/resource/csgo_thai.txt,  M csgo/steam.inf,  M update.sh
    https://steamdb.info/patchnotes/3113110/

--- update.sh
+++ update.sh
@@ -11,3 +11,5 @@ ProcessVPK
 FixUCS2
 
 CreateCommit "$(grep "ClientVersion=" csgo/steam.inf | grep -o '[0-9\.]*') | $(grep "PatchVersion=" csgo/steam.inf | grep -o '[0-9\.]*')" "$1"
+
+bash ~/csgo_panorama/update.sh
