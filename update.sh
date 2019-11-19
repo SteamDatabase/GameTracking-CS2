fde71e7602e09cd960a0ebe3da757ff0d0127e60
Wendy <freeman@steamdb.info>
2019-11-19 12:40:07 +0000

    Track code.pbin file list (not actual content due to dmca)

--- update.sh
+++ update.sh
@@ -8,6 +8,8 @@ echo "Processing CS:GO..."
 ProcessDepot ".so"
 ProcessVPK
 
+unzip -lv ./csgo/panorama/code.pbin > ./csgo/panorama/code_pbin.txt
+
 FixUCS2
 
 CreateCommit "$(grep "ClientVersion=" csgo/steam.inf | grep -o '[0-9\.]*') | $(grep "PatchVersion=" csgo/steam.inf | grep -o '[0-9\.]*')" "$1"
