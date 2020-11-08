9a9930c7a020e89116b57f4124c745490b840e73
Wendy <freeman@steamdb.info>
2020-11-08 11:20:07 +0000

    1190 | 1.37.7.0 | 38 files | M  .gitignore, A  csgo/maps/ar_baggage.lmp, A  csgo/maps/ar_dizzy.lmp, A  csgo/maps/ar_lunacy.lmp, A  csgo/maps/ar_monastery.lmp, A  csgo/maps/ar_shoots.lmp, A  csgo/maps/coop_kasbah.lmp, A  csgo/maps/cs_agency.lmp, A  csgo/maps/cs_assault.lmp, A  csgo/maps/cs_italy.lmp, A  csgo/maps/cs_militia.lmp, A  csgo/maps/cs_office.lmp, A  csgo/maps/de_anubis.lmp, A  csgo/maps/de_bank.lmp, A  csgo/maps/de_cache.lmp, A  csgo/maps/de_canals.lmp, A  csgo/maps/de_cbble.lmp, A  csgo/maps/de_dust2.lmp, A  csgo/maps/de_inferno.lmp, A  csgo/maps/de_lake.lmp, A  csgo/maps/de_mirage.lmp, A  csgo/maps/de_mutiny.lmp, A  csgo/maps/de_nuke.lmp, A  csgo/maps/de_overpass.lmp, A  csgo/maps/de_safehouse.lmp, A  csgo/maps/de_shortdust.lmp, A  csgo/maps/de_shortnuke.lmp, A  csgo/maps/de_stmarc.lmp, A  csgo/maps/de_sugarcane.lmp, A  csgo/maps/de_swamp.lmp, A  csgo/maps/de_train.lmp, A  csgo/maps/de_vertigo.lmp, A  csgo/maps/dz_blacksite.lmp, A  csgo/maps/dz_sirocco.lmp, A  csgo/maps/gd_cbble.lmp, A  csgo/maps/gd_rialto.lmp, A  csgo/maps/tr
    https://steamdb.info/patchnotes/5780092/

--- update.sh
+++ update.sh
@@ -10,6 +10,11 @@ ProcessVPK
 
 unzip -lv ./csgo/panorama/code.pbin > ./csgo/panorama/code_pbin.txt
 
+while IFS= read -r -d '' file
+do
+	/home/steamdb/EntityLumpDumper/EntityLumpDumper/bin/Release/EntityLumpDumper "$file"
+done <   <(find "csgo/maps/" -type f -name "*.bsp" -print0)
+
 FixUCS2
 
 CreateCommit "$(grep "ClientVersion=" csgo/steam.inf | grep -o '[0-9\.]*') | $(grep "PatchVersion=" csgo/steam.inf | grep -o '[0-9\.]*')" "$1"
