1fa5725bc39032712fa669d84639c44616554b33
Wendy <freeman@steamdb.info>
2022-12-17 11:43:09 +0000

    1539 | 1.38.5.0 | 67 files | D csgo/maps/ar_baggage.lmp,  D csgo/maps/ar_dizzy.lmp,  D csgo/maps/ar_lunacy.lmp,  D csgo/maps/ar_monastery.lmp,  D csgo/maps/ar_shoots.lmp,  D csgo/maps/coop_autumn.lmp,  D csgo/maps/coop_fall.lmp,  D csgo/maps/coop_kasbah.lmp,  D csgo/maps/cs_agency.lmp,  D csgo/maps/cs_apollo.lmp,  D csgo/maps/cs_assault.lmp,  D csgo/maps/cs_climb.lmp,  D csgo/maps/cs_insertion2.lmp,  D csgo/maps/cs_italy.lmp,  D csgo/maps/cs_militia.lmp,  D csgo/maps/cs_office.lmp,  D csgo/maps/de_ancient.lmp,  D csgo/maps/de_anubis.lmp,  D csgo/maps/de_bank.lmp,  D csgo/maps/de_basalt.lmp,  D csgo/maps/de_blagai.lmp,  D csgo/maps/de_boyard.lmp,  D csgo/maps/de_breach.lmp,  D csgo/maps/de_cache.lmp,  D csgo/maps/de_calavera.lmp,  D csgo/maps/de_canals.lmp,  D csgo/maps/de_cbble.lmp,  D csgo/maps/de_chalice.lmp,  D csgo/maps/de_crete.lmp,  D csgo/maps/de_dust2.lmp,  D csgo/maps/de_elysion.lmp,  D csgo/maps/de_engage.lmp,  D csgo/maps/de_extraction.lmp,  D csgo/maps/de_grind.lmp,  D csgo/maps/de_guard.lmp,  D csgo/maps/de_hive.lmp,  D csgo

--- update.sh
+++ update.sh
@@ -10,12 +10,6 @@ ProcessVPK
 
 unzip -lv ./csgo/panorama/code.pbin > ./csgo/panorama/code_pbin.txt
 
-while IFS= read -r -d '' file
-do
-	# https://github.com/xPaw/EntityLumpDumper
-	../.support/EntityLumpDumper/EntityLumpDumper/bin/Release/linux-x64/publish/EntityLumpDumper "$file"
-done <   <(find "csgo/maps/" -type f -name "*.bsp" -print0)
-
 FixUCS2
 
 CreateCommit "$(grep "ClientVersion=" csgo/steam.inf | grep -o '[0-9\.]*') | $(grep "PatchVersion=" csgo/steam.inf | grep -o '[0-9\.]*')" "$1"
