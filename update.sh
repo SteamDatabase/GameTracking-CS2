c2d3aeee15aebcd845bf1070a0c39293b78fd544
Wendy <freeman@steamdb.info>
2023-05-04 23:40:54 +0000

    2000064 (CSGO: 1563) | 38 files | M csgo/bin/linux64/client_client.txt,  M csgo/bin/linux64/client_client_strings.txt,  M csgo/bin/linux64/server_client_strings.txt,  M csgo/bin/server_strings.txt,  M csgo/pak01_dir.txt,  M csgo/panorama/code_pbin.txt,  M csgo/resource/csgo_brazilian.txt,  M csgo/resource/csgo_bulgarian.txt,  M csgo/resource/csgo_czech.txt,  M csgo/resource/csgo_danish.txt,  M csgo/resource/csgo_dutch.txt,  M csgo/resource/csgo_english.txt,  M csgo/resource/csgo_finnish.txt,  M csgo/resource/csgo_french.txt,  M csgo/resource/csgo_german.txt,  M csgo/resource/csgo_greek.txt,  M csgo/resource/csgo_hungarian.txt,  M csgo/resource/csgo_italian.txt,  M csgo/resource/csgo_japanese.txt,  M csgo/resource/csgo_korean.txt,  M csgo/resource/csgo_koreana.txt,  M csgo/resource/csgo_latam.txt,  M csgo/resource/csgo_norwegian.txt,  M csgo/resource/csgo_polish.txt,  M csgo/resource/csgo_portuguese.txt,  M csgo/resource/csgo_romanian.txt,  M csgo/resource/csgo_russian.txt,  M csgo/resource/csgo_schinese.txt,  M csgo/resource/csgo_spanish.txt,
    https://steamdb.info/patchnotes/11159564/

--- update.sh
+++ update.sh
@@ -14,5 +14,3 @@ unzip -lv ./csgo/panorama/code.pbin > ./csgo/panorama/code_pbin.txt
 FixUCS2
 
 CreateCommit "$(grep "ClientVersion=" game/csgo/steam.inf | grep -o '[0-9\.]*') (CSGO: $(grep "ClientVersion=" csgo/steam.inf | grep -o '[0-9\.]*'))" "$1"
-
-bash ~/csgo_panorama/update.sh
