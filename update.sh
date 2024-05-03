80cd467f43571e979899b3dece753315ab9aeca8
Wendy <freeman@steamdb.info>
2024-05-03 11:48:00 +0000

    2000310 | 15 files | M update.sh, ?? game/core/pak01_dir/scripts/tools/, ?? game/core/pak01_dir/soundstacks/dsp_mix_modifiers.vdata, ?? game/core/pak01_dir/textures/, ?? game/csgo/pak01_dir/items/, ?? game/csgo/pak01_dir/scripts/light_style_event_types.vdata, ?? game/csgo/pak01_dir/scripts/light_styles.vdata, ?? game/csgo/pak01_dir/scripts/nav_hulls.vdata, ?? game/csgo/pak01_dir/scripts/nav_hulls_presets.vdata, ?? game/csgo/pak01_dir/scripts/navlinks.vdata, ?? game/csgo/pak01_dir/scripts/precipitation.vdata, ?? game/csgo/pak01_dir/scripts/propdata.vdata, ?? game/csgo/pak01_dir/scripts/tools/, ?? game/csgo/pak01_dir/scripts/weapons.vdata, ?? game/csgo/pak01_dir/soundstacks/dsp_mix_modifiers.vdata
    https://steamdb.info/patchnotes/14249031/

--- update.sh
+++ update.sh
@@ -19,7 +19,7 @@ do
 		--output "$(echo "$file" | sed -e 's/\.vpk$/\//g')" \
 		--vpk_cache \
 		--vpk_decompile \
-		--vpk_extensions "txt,lua,kv3,db,gameevents,vcss_c,vjs_c,vts_c,vxml_c,vsndevts_c,vsndstck_c,vpulse_c" \
+		--vpk_extensions "txt,lua,kv3,db,gameevents,vcss_c,vjs_c,vts_c,vxml_c,vsndevts_c,vsndstck_c,vpulse_c,vdata_c" \
 	|| echo "Decompiler failed"
 done <   <(find . -type f -name "pak01_dir.vpk" -print0)
 
