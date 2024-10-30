a8ae8be2ee832ccd01410c8672a9f73bffecc2af
Wendy <freeman@steamdb.info>
2024-10-30 00:16:08 +0000

    2000425 | 8 files | M DumpSource2/convars.txt,  M DumpSource2/schemas/server/CCSPlayerController_DamageServices.h,  M game/bin/built_from_cl.txt,  M game/csgo/bin/linuxsteamrt64/libclient_strings.txt,  M game/csgo/bin/linuxsteamrt64/libserver_strings.txt,  M game/csgo/pak01_dir.txt,  M game/csgo/readonly_tools_asset_info.txt,  M game/csgo/steam.inf
    https://steamdb.info/patchnotes/16228412/

--- DumpSource2/schemas/server/CCSPlayerController_DamageServices.h
+++ DumpSource2/schemas/server/CCSPlayerController_DamageServices.h
@@ -1,6 +1,5 @@
 class CCSPlayerController_DamageServices : public CPlayerControllerComponent
 {
-	CUtlVector< CDamageRecord > m_DamageListServer;
 	int32 m_nSendUpdate;
 	CUtlVectorEmbeddedNetworkVar< CDamageRecord > m_DamageList;
 };
