4211e03979b9f3bfcd4d6e7721e3ae8d50d4c9ac
Wendy <freeman@steamdb.info>
2025-03-04 00:22:46 +0000

    2000495 | 16 files | M DumpSource2/convars.txt,  M game/bin/built_from_cl.txt,  M game/bin/linuxsteamrt64/libengine2_strings.txt,  M game/bin/linuxsteamrt64/libpanoramauiclient_strings.txt,  M game/core/pak01_dir.txt,  M game/csgo/bin/linuxsteamrt64/libclient_strings.txt,  M game/csgo/pak01_dir.txt,  M game/csgo/pak01_dir/panorama/layout/premier_season_progress.xml,  M game/csgo/pak01_dir/panorama/scripts/premier_season_progress.js,  M game/csgo/pak01_dir/panorama/styles/popups/popup_acknowledge_item.css,  M game/csgo/pak01_dir/panorama/styles/premier_season_progress.css,  M game/csgo/pak01_dir/panorama/styles/settings/settings.css,  M game/csgo/pak01_dir/resource/csgo_english.txt,  M game/csgo/shaders_pc_dir.txt,  M game/csgo/shaders_vulkan_dir.txt,  M game/csgo/steam.inf
    https://steamdb.info/patchnotes/17581511/

--- game/csgo/pak01_dir/panorama/scripts/premier_season_progress.js
+++ game/csgo/pak01_dir/panorama/scripts/premier_season_progress.js
@@ -1,8 +1,10 @@
 "use strict";
 /// <reference path="csgo.d.ts" />
 /// <reference path="rating_emblem.ts" />
+/// <reference path="common/formattext.ts" />
 var SeasonProgress;
 (function (SeasonProgress) {
+    const _m_nWinsForMedal = 25;
     function _Init() {
         SetRating();
     }
@@ -34,15 +36,14 @@ var SeasonProgress;
                             0;
         nBars = nBars < 5 ? nBars + 1 : 5;
         let elParent = $.GetContextPanel().FindChildInLayoutFile('id-premier-season-bars');
-        let numMatchesPerBar = 25;
         for (let i = 1; i <= nBars; i++) {
             let elBar = elParent.FindChild('bar-' + i);
             if (!elBar) {
                 elBar = $.CreatePanel('Panel', elParent, 'bar-' + i);
                 elBar.BLoadLayoutSnippet('one-bar');
             }
-            let rangeOfMatchesInBar = { min: i == 1 ? 1 : ((i - 1) * numMatchesPerBar), max: (i * numMatchesPerBar) };
-            let widthInnerBar = (nWins >= (rangeOfMatchesInBar.max - 1)) ? 1 : ((nWins - rangeOfMatchesInBar.min) / (numMatchesPerBar - 1));
+            let rangeOfMatchesInBar = { min: i == 1 ? 1 : ((i - 1) * _m_nWinsForMedal), max: (i * _m_nWinsForMedal) };
+            let widthInnerBar = (nWins >= (rangeOfMatchesInBar.max - 1)) ? 1 : ((nWins - rangeOfMatchesInBar.min) / (_m_nWinsForMedal - 1));
             elBar.FindChildInLayoutFile('id-inner-bar').style.width = (widthInnerBar * 100) + '%';
             elBar.FindChildInLayoutFile('id-inner-bar').SwitchClass('tier', 'rank-tier-' + color);
             elBar.SwitchClass('num-bars', nBars + '-bars');
@@ -54,6 +55,50 @@ var SeasonProgress;
         $.GetContextPanel().FindChildInLayoutFile('id-medal-end').itemid = itemId;
         $.GetContextPanel().SetDialogVariableInt('wins', nWins);
         $.GetContextPanel().SetDialogVariableInt('threshold', nBars * 25);
+        _ShowHideExpirationWarning();
+        _SetInfoIconTooltip();
+    }
+    function _ShowHideExpirationWarning() {
+        let nTime = MyPersonaAPI.GetPipRankExpiration("Premier");
+        let nWins = MyPersonaAPI.GetPipRankWins("Premier");
+        let elParent = $.GetContextPanel().FindChildInLayoutFile('id-premier-bar-container');
+        if (nWins < _m_nWinsForMedal || nTime >= 0) {
+            elParent.SetHasClass('show-warning', false);
+            let elImages = elParent.FindChildInLayoutFile('id-premier-bar-icons');
+            elImages.SetPanelEvent('onmouseover', () => {
+                return;
+            });
+            elImages.SetPanelEvent('onmouseout', () => {
+                return;
+            });
+            return;
+        }
+        if (nTime < 0) {
+            elParent.SetHasClass('show-warning', true);
+            let elImages = elParent.FindChildInLayoutFile('id-premier-bar-icons');
+            elImages.SetPanelEvent('onmouseover', () => {
+                UiToolkitAPI.ShowTextTooltip('id-premier-bar-icons', '#season_progress_rating_expired');
+            });
+            elImages.SetPanelEvent('onmouseout', () => {
+                UiToolkitAPI.HideTextTooltip();
+            });
+        }
+    }
+    function _SetInfoIconTooltip() {
+        let nTime = MyPersonaAPI.GetPipRankExpiration("Premier");
+        let nWins = MyPersonaAPI.GetPipRankWins("Premier");
+        let elTooltip = $.GetContextPanel().FindChildInLayoutFile('id-season-progress-tooltip');
+        let sTooltip = $.Localize('#season_progress_tooltip-body');
+        if (nWins >= _m_nWinsForMedal && nTime > 0) {
+            elTooltip.SetDialogVariable('time', FormatText.SecondsToSignificantTimeString(nTime));
+            sTooltip = sTooltip + $.Localize('#season_progress_tooltip-expiration_time', elTooltip);
+        }
+        elTooltip.SetPanelEvent('onmouseover', () => {
+            UiToolkitAPI.ShowTitleTextTooltip('id-season-progress-tooltip', '#season_progress_tooltip-title', sTooltip);
+        });
+        elTooltip.SetPanelEvent('onmouseout', () => {
+            UiToolkitAPI.HideTitleTextTooltip();
+        });
     }
     function ReadyForDisplay() {
         SetRating();
