529d69b1db6f36dae3e81eca64860fa695133fd4
Wendy <freeman@steamdb.info>
2025-08-23 08:48:31 +0000

    cleanup

--- game/csgo/pak01_dir/panorama/scripts/common/panelregistration.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/// <reference path="../csgo.d.ts" />
-(function () {
-    UiToolkitAPI.RegisterPanel2d('CSGOPerfTestsJsMultipleContexts', 'file://{resources}/layout/tests/perf/perf_jsmultiplecontexts.xml');
-    UiToolkitAPI.RegisterPanel2d('CSGOPerfTestsJsSingleContext', 'file://{resources}/layout/tests/perf/perf_jssinglecontext.xml');
-    UiToolkitAPI.RegisterPanel2d('CSGOPerfTestsTypeSafety', 'file://{resources}/layout/tests/perf/type_safety.xml');
-    UiToolkitAPI.RegisterPanel2d('ControlLibTestPanel', 'file://{resources}/layout/tests/controllibtestpanel.xml');
-})();
--- game/csgo/pak01_dir/panorama/scripts/hud/hudhealthammocenter.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-/// <reference path="../csgo.d.ts" />
-/// <reference path="../common/anim_background.ts" /> 
-(function () {
-})();
