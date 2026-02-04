3049e8f919ff75da29ea043e564cae08b395565d
Wendy <freeman@steamdb.info>
2026-02-04 23:37:01 +0000

    2000728 | 13 files | M DumpSource2/schemas/server/CGameRules.h,  M Protobufs/source2_steam_stats.proto,  M game/bin/built_from_cl.txt,  M game/bin/linuxsteamrt64/libengine2_strings.txt,  M game/bin/linuxsteamrt64/libnetworksystem_strings.txt,  M game/bin/linuxsteamrt64/libvphysics2_strings.txt,  M game/csgo/bin/linuxsteamrt64/libclient_strings.txt,  M game/csgo/bin/linuxsteamrt64/libserver_strings.txt,  M game/csgo/pak01_dir.txt,  M game/csgo/pak01_dir/maps/workshop_compatibility_versions.txt,  M game/csgo/pak01_dir/scripts/workshop_cvar_whitelist.txt,  M game/csgo/steam.inf,  M game/csgo_community_addons/de_poseidon/de_poseidon_dir.txt
    https://steamdb.info/patchnotes/21789745/

--- DumpSource2/schemas/server/CGameRules.h
+++ DumpSource2/schemas/server/CGameRules.h
@@ -7,6 +7,8 @@ class CGameRules
 	CNetworkVarChainer __m_pChainEntity;
 	char[128] m_szQuestName;
 	int32 m_nQuestPhase;
+	uint32 m_nLastMatchTime;
+	uint64 m_nLastMatchTime_MatchID64;
 	// MNetworkEnable
 	int32 m_nTotalPausedTicks;
 	// MNetworkEnable
